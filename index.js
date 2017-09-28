302fb836e404cb052d404a3b52c75e28f9fe1aa0
jennife <jennifer@melten.com>
2017-09-28 17:42:05 +0800

    server/page, mysql to be tested

---
+++ index.js
@@ -0,0 +1,44 @@
+var http =require('http');
+var fs = require('fs');
+//var nodemailer = require('nodemailer');
+var mysql = require('mysql');
+
+
+var server = http.createServer( page);
+
+function page(req, res){
+    if(req.url == '/abc'){
+        console.log('create a server')
+        res.end('ready');
+    }//end if
+    else{
+        res.end('not exist');
+    }
+
+}
+
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'neal',
+    password: 'neal',
+    database:'melten',
+    port:3306
+
+})
+
+
+function traerMysql(requesst, response){
+var query = connection.query(' select * from call',
+ function(error, result){
+     if (error) {throw error}
+     else {
+         var res= result;
+         response.end(JSON.stringify(res));
+     }
+
+}//function
+)// connection.query
+}//traerMysql
+
+
+server.listen(8079);
